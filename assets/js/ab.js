40b19db03bfc72ceb8182e795a56f2bc551c4352
Md. Saiful Islam <73848027+saifpust@users.noreply.github.com>
2025-05-10 15:18:04 +0600

    Header updated

--- assets/js/ab.js
+++ assets/js/ab.js
@@ -1,12 +1,12 @@
 
 
-const common_header = document.querySelector("#cc-header");
+const ab_header = document.querySelector(".cc-ab-header");
 
-if (common_header) {
+if (ab_header) {
     const hamburger_menu_open = document.querySelector(".hamburger-menu");
 
     hamburger_menu_open.addEventListener("click", function () {
-        common_header.classList.toggle("cc-mobile-menu-active");
+        ab_header.classList.toggle("cc-mobile-menu-active");
     });
 }
 
